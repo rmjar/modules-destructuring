ceecac33557cf7b1597b1a0c8ec3b7cb2760a9e8
Michał Różański <rozanski.michal.mr@gmail.com>
2018-11-24 09:12:23 +0100

    Excercise 1

--- 01-string_template/script.js
+++ 01-string_template/script.js
@@ -1,5 +1,16 @@
-const cardTemplate = () => `
-  <h1>Zastąp mnie</h1>
+const cardTemplate = (myRobot) => `
+<div class="w-25 px-1 mb-2">
+<div class="card">
+  <img class="card-img-top" src=${myRobot.avatar}>
+
+  <div class="card-body">
+    <h5 class="card-title">${myRobot.name}</h5>
+
+    <p class="card-text">${myRobot.caption}</p>
+    <a href="#" class="btn btn-primary ${myRobot.avaliable ? 'enabled' : 'disabled'}">Buy now!</a>
+  </div>
+</div>
+</div>
 `;
 
 const container = document.getElementById('container');
@@ -11,4 +22,4 @@ const robot = {
   avaliable: false
 };
 
-container.insertAdjacentHTML('beforeend', cardTemplate(robot));
+dataset.forEach(robot => container.insertAdjacentHTML('beforeend', cardTemplate(robot)));
