be85226fd1a5fa9c1ecb7c4b8d8eafa1844500b2
Krzyku <jasinski.it@gmail.com>
2018-11-24 08:53:35 +0100

    solve first task template string

--- 01-string_template/script.js
+++ 01-string_template/script.js
@@ -1,5 +1,16 @@
-const cardTemplate = () => `
-  <h1>Zastąp mnie</h1>
+const cardTemplate = (myRobot) => `
+  <div class="w-25 px-1 mb-2">
+    <div class="card">
+      <img class="card-img-top" src="${myRobot.avatar}">
+
+      <div class="card-body">
+        <h5 class="card-title">${myRobot.name}</h5>
+    
+        <p class="card-text">Miejsce na opis robota</p>
+        <a href="#" class="btn btn-primary ${myRobot.avaliable ? '' : 'disabled'}">Buy now!</a>
+      </div>
+    </div>
+  </div>
 `;
 
 const container = document.getElementById('container');
@@ -11,4 +22,6 @@ const robot = {
   avaliable: false
 };
 
-container.insertAdjacentHTML('beforeend', cardTemplate(robot));
+dataset.forEach(item => {
+  container.insertAdjacentHTML('beforeend', cardTemplate(item));
+})
